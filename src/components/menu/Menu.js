fa4ad4df699411e291dc2fc6331b084744de511d
Andrii Zvarych <zvaruch2002@gmail.com>
2024-05-07 14:05:55 +0300

    Add Menu

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, Container } from "react-bootstrap";
+const Menu = () => {
+return (
+<Navbar>
+<Container>
+<Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+<Navbar.Toggle />
+<Navbar.Collapse className="justify-content-end">
+<Navbar.Text>
+Signed in as: <a href="#login">Mark Otto</a>
+</Navbar.Text>
+</Navbar.Collapse>
+</Container>
+</Navbar>
+);
+};
+export default Menu;
